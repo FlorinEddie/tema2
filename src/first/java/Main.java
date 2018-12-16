5089245d3453b7adc39212f9307ede8c601fe676
FlorinG <eddie07_ro@yahoo.com>
2018-12-16 09:25:29 +0200

    Am adaugat altceva

--- src/first/java/Main.java
+++ src/first/java/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Merry Christmas!!!");
+        System.out.println("La multi ani");
+        System.out.println("Un an nou fericit");
     }
 }
